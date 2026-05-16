274be2cd614d9860c897ffe0b990f3dfe9e0118a
brahimhatim265 <brahimhatim265@gmail.com>
2026-05-16 03:08:43 +0100

    cree User-Service

---
+++ microservices/user-service/src/main/java/com/social/userservice/entities/User.java
@@ -0,0 +1,42 @@
+package com.social.userservice.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false)
+    private String role = "USER"; // USER, ORGANIZER, ADMIN
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<Event> organizedEvents = new ArrayList<>();
+    
+    public String getUsername() {
+        return this.email;
+    }
+}
